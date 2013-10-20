cb861860bce4bf40653b7343f353408ab0b52bdf
atrullmdsol <atrull@mdsol.com>
2013-10-20 21:33:07 +0100

    Improved logging / less redundant dumping of information.

--- recipes/install.rb
+++ recipes/install.rb
@@ -33,13 +33,13 @@ else
    
     # No devices found 
 
-    log "No Ephemeral devices found"
+    log "Ephemeral devices not found"
 
   elsif ( ephemeral_devices.length >= 2 ) || ( ephemeral_devices.length == 1 && node[:ephemeral][:raid][:force] = true )
 
     # We have more than one, or we have one and we are forcing it.
 
-    log "Attempting to create array out of : #{ephemeral_devices.inspect}"
+    log "Ephemeral devices have been found"
 
     package "mdadm"
 
